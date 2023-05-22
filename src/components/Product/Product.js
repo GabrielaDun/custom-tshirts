0563f7da098789ba7449523bb49c9b18be5d0285
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-22 12:53:04 +0200

    Created an automatic render of products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Button from '../Button/Button';
 
 const Product = props => {
+  console.log(props);
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
@@ -13,8 +14,8 @@ const Product = props => {
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {props.basePrice}</span>
         </header>
         <form>
           <div className={styles.sizes}>
