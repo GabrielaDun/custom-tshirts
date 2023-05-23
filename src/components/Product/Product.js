d4962229714423a059a02e59cb7bf340c1c4c176
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-23 10:02:02 +0200

    Created functionality that updates price based on t-shirt size chosen by the user

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,16 +7,19 @@ import { useState } from 'react';
 const Product = props => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+  const [currentAddedPrice, setCurrentAddedPrice] = useState(0);
   console.log(currentSize);
   console.log(currentColor);
+  console.log(currentAddedPrice);
   
 
   const capitalLetterColors = color => {
     return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
   }
 
-
-
+  const getPrice = () => {
+      return (props.basePrice + currentAddedPrice );
+  }
 
   return (
     <article className={styles.product}>
@@ -29,16 +32,20 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {props.basePrice}</span>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
               {props.sizes.map(size =>
-                <li key={size.name}><button type="button">
-                  {size.name}
-                </button></li>
+                <li key={size.name}>
+                  <button 
+                    type="button"
+                    onClick={() => {setCurrentSize(size); setCurrentAddedPrice(size.additionalPrice)}}>
+                    {size.name}
+                  </button>
+                </li>
                 )}
             </ul>
           </div>
